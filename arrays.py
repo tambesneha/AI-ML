9ab78d5cec2757400a1db188778473d2cd9cd607
tambesneha <139611274+tambesneha@users.noreply.github.com>
2026-06-19 08:48:28 +0200

    Delete arrays.py

--- arrays.py
+++
@@ -1,8 +0,0 @@
-import numpy as np
-
-arr1 = np.array([[1 , 2 , 3 ],
-                [4 ,5 , 6],
-                ])
-print(arr1)
-print("number of dimensions:" , arr1.ndim)
-print("shape(rows , columns)" , arr1.shape)
